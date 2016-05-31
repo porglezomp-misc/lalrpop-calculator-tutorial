078fc1fcb7db4c3bb90bd686c48189ae16530049
Caleb Jones <self@calebjones.net>
2016-05-31 18:16:03 -0400

    Improve the tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 pub mod calculator1;
-pub mod calculator2;
-pub mod calculator3;
-pub mod calculator4;
-pub mod ast;
 
 #[test]
 fn calculator1() {
@@ -11,6 +7,8 @@ fn calculator1() {
     assert!(calculator1::parse_Term("((22)").is_err());
 }
 
+pub mod calculator2;
+
 #[test]
 fn calclulator2() {
     assert!(calculator2::parse_Term("22").is_ok());
@@ -18,18 +16,28 @@ fn calclulator2() {
     assert!(calculator2::parse_Term("((22)").is_err());
 }
 
+pub mod calculator3;
+
+macro_rules! test3 {
+    ($expr:expr) => {
+        assert_eq!(calculator3::parse_Expr(stringify!($expr)).unwrap(), $expr);
+    }
+}
 #[test]
 fn calculator3() {
-    assert_eq!(calculator3::parse_Expr("22").unwrap(), 22);
-    assert_eq!(calculator3::parse_Expr("11*2").unwrap(), 11*2);
-    assert_eq!(calculator3::parse_Expr("1+2*3").unwrap(), 1+2*3);
-    assert_eq!(calculator3::parse_Expr("1-1-1").unwrap(), 1-1-1);
-    assert_eq!(calculator3::parse_Expr("(3+3)*(4+3)").unwrap(), (3+3)*(4+3));
-    assert_eq!(calculator3::parse_Expr("((3-8)/2+1)").unwrap(), ((3-8)/2)+1);
+    test3!(22);
+    test3!(11 * 2);
+    test3!(1 + 2 * 3);
+    test3!(1 - 1 - 1);
+    test3!((3 + 3) * (4 + 3));
+    test3!(((3 - 8)/2 + 1));
     assert!(calculator3::parse_Expr("3*(/3)").is_err());
     assert!(calculator3::parse_Expr("*3").is_err());
     assert!(calculator3::parse_Expr("(").is_err());
 }
+
+pub mod calculator4;
+pub mod ast;
 
 #[test]
 fn calculator4() {
@@ -37,6 +45,7 @@ fn calculator4() {
                "((22 * 44) + 66)");
 }
 
+#[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
 }
